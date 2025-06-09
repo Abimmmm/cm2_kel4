2a64071e6363c07d684710a0209be5de6ccc349d
meeeeeeeeeeee <sitinikmatussholihahdivti@gmail.com>
2025-06-09 22:35:29 +0700

    Update AntrianDoubleLinkedList.java

--- AntrianDoubleLinkedList.java
+++ AntrianDoubleLinkedList.java
@@ -1,5 +1,35 @@
 public class AntrianDoubleLinkedList {
     Node head, tail;
 
+    public boolean isEmpty(){
+        return head == null;
+    }
+
+    public void tampilkanAntrian(){
+        if (isEmpty()) {
+            System.out.println("Antrian kosong.");
+            return;
+        }
+
+        NodeKendaraan temp = head;
+        System.out.println("-- Daftar Kendaraan dalam Antrian --");
+        while (temp != null) {
+            System.out.println("Plat Nomor : " + temp.kendaraan.getPlatNomor());
+            System.out.println("Jenis      : " + temp.kendaraan.getJenisKendaraan());
+            System.out.println("Merk       : " + temp.kendaraan.getMerk());
+            System.out.println("-----------------------------");
+            temp = temp.next;
+        }
+    }
+
+    public Kendaraan dequeue(){
+        if (head == null) return null;
+        
+        Kendaraan k = head.kendaraan;
+        head = head.next;
+
+        if (head == null) tail = null;
+        return k;
+    } 
     
 }
