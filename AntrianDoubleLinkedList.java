8cad9dfc0468e6a95040f95bf64f3994ac9da1a5
meeeeeeeeeeee <sitinikmatussholihahdivti@gmail.com>
2025-06-09 23:29:36 +0700

    Update AntrianDoubleLinkedList.java

--- AntrianDoubleLinkedList.java
+++ AntrianDoubleLinkedList.java
@@ -11,25 +11,37 @@ public class AntrianDoubleLinkedList {
             return;
         }
 
-        NodeKendaraan temp = head;
-        System.out.println("-- Daftar Kendaraan dalam Antrian --");
+         NodeKendaraan temp = head;
+        System.out.println("-- Daftar Kendaraan dalam Antrian");
         while (temp != null) {
-            System.out.println("Plat Nomor : " + temp.kendaraan.getPlatNomor());
-            System.out.println("Jenis      : " + temp.kendaraan.getJenisKendaraan());
-            System.out.println("Merk       : " + temp.kendaraan.getMerk());
+            System.out.println("Plat Nomor : " + temp.kendaraan.platNomor);
+            System.out.println("Jenis      : " + temp.kendaraan.jenisKendaraan);
+            System.out.println("Merk       : " + temp.kendaraan.merk);
             System.out.println("-----------------------------");
             temp = temp.next;
         }
     }
 
+   public void enqueue(Kendaraan k){
+        NodeKendaraan baru = new NodeKendaraan(k);
+        if (head == null) {
+            head = tail = baru;
+        } else {
+            tail.next = baru;
+            baru.prev = tail;
+            tail = baru;
+        }
+    }
+
     public Kendaraan dequeue(){
         if (head == null) return null;
-        
+
         Kendaraan k = head.kendaraan;
         head = head.next;
+        if (head != null) head.prev = null;
+        else tail = null;
 
-        if (head == null) tail = null;
         return k;
-    } 
+    }
     
 }
